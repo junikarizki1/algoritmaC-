db89b9a4f51623969bdec9f9abcbf918d551d30c
junikarizki1 <117917208+junikarizki1@users.noreply.github.com>
2022-11-17 19:36:45 +0700

    Create s41 manipulator setfill().cpp

---
+++ s41 manipulator setfill().cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout <<"Tampilan tanpa menggunakan setfill()" <<endl;
+    cout <<setw(4) <<1 <<endl <<endl; 
+    cout <<"Tampilan menggunakan setfill('*')" <<endl;
+    cout <<setfill('*') <<setw(4) <<1 <<endl;
+}
