4df4db834d4863d6cfd093672b241fefdd00bb65
junikarizki1 <117917208+junikarizki1@users.noreply.github.com>
2022-11-17 19:27:28 +0700

    Create s40 manipulator setw dan endl.cpp

---
+++ s40 manipulator setw dan endl.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+  cout <<setw(4) <<1 <<endl;
+  cout <<setw(4) <<12 <<endl;
+  cout <<setw(4) <<123 <<endl;
+  cout <<setw(4) <<1234 <<endl;
+  cout <<setw(4) <<12345 <<endl;
+  cout <<setw(4) <<123456 <<endl;
+   
+}
