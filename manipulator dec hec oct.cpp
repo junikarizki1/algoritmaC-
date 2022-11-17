6ac78a0272e3f96e3af568cda980b71379b9636e
junikarizki1 <117917208+junikarizki1@users.noreply.github.com>
2022-11-17 19:50:34 +0700

    Create manipulator dec hec oct.cpp

---
+++ manipulator dec hec oct.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout <<"24 dalam desimal tercetak " <<dec <<24 <<endl;
+    cout <<"24 dalam oktal tercetak " <<oct <<24 <<endl;
+    cout <<"24 dalam heksadesimal tercetak " <<hex <<24 <<endl;
+    cout <<"setelah memakai manipulator hex " <<endl;
+    cout <<"jika mencetak lagi bilangan 24, maka akan mencetak " <<24 <<endl;
+    
+    return 0;
+}
