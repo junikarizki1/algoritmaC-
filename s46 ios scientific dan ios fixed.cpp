7915e9326202ff2286a06be4474c7364a84417c2
xenuver <auikwok2@gmail.com>
2022-11-18 19:43:54 +0700

    Memperbaiki ios Fixed s46

--- s46 ios scientific dan ios fixed.cpp
+++ s46 ios scientific dan ios fixed.cpp
@@ -9,16 +9,17 @@ int main()
     cout <<"Tanpa penggunaan ios " <<endl;
     cout <<"123.45 --> " <<123.45 <<endl;
     cout <<"12345 --> " <<12345 <<endl;
-    
+
     //seting ios
     cout <<setiosflags(ios :: scientific); //set ios scientific
     cout <<"Menggunakan ios :: scientific " <<endl;
     cout <<"123.45 --> "<<123.45 <<endl;
-    cout <<"12345 --> " <<12345 <<endl; 
+    cout <<"12345 --> " <<12345 <<endl;
+    cout <<resetiosflags(ios :: scientific);
     cout <<setiosflags(ios :: fixed); //set ios fixed
     cout <<"Menggunakan ios :: fixed " <<endl;
     cout <<"123.45 --> "<<123.45 <<endl;
-    cout <<"12345 --> " <<12345 <<endl; 
-    
+    cout <<"12345 --> " <<12345 <<endl;
+
   return 0;
 }
