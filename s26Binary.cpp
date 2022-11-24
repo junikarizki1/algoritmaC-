87d77ed688c33f7372b27ea7a3174bd02a3d094e
Junika Rizki <117917208+junikarizki1@users.noreply.github.com>
2022-11-24 21:34:02 +0700

    Create s26Binary.cpp

---
+++ s26Binary.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{    
+    cout <<"7 * 2   = "<< 7 * 2 <<endl;
+    cout <<"7 / 2   = "<< 7 / 2 <<endl;
+    cout <<"7.0 / 2 = "<<7.0 / 2 <<endl;
+    cout <<"7 % 2 = "<<7 % 2 <<endl;
+    
+    return 0;
+}
