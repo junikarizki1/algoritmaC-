bd5a34d88fc91b2b6cf1dc916f1a728d6e10b8db
Junika Rizki <117917208+junikarizki1@users.noreply.github.com>
2022-11-24 21:21:15 +0700

    Create s26Const.cpp

---
+++ s26Const.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*const float phi = 3.14;
+    float r, l, k;
+    r = 7.0;
+    l = 0.5 * phi * r * r;
+    k = 2 * phi * r;
+    
+    cout << "Luas Lingkaran    : "<<l <<endl;
+    cout <<"Keliling Lingkaran : "<<k <<endl;*/
+    
+    
+    return 0;
+}
