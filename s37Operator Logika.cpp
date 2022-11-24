7e8a7b8fff7f98e9650381f05d019ac28aff2845
Junika Rizki <117917208+junikarizki1@users.noreply.github.com>
2022-11-24 21:47:47 +0700

    Create s37Operator Logika.cpp

---
+++ s37Operator Logika.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{    
+    cout <<"(3<2) && (4>3) -->" <<((3<2) && (4>3)) <<endl;
+    cout <<"(3<2) || (4>3) -->" <<((3<2) || (4>3)) <<endl;
+    cout <<"!(3<2)         -->" <<(!(3<2))<<endl;
+    return 0;
+}
