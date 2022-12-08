22a0d8f66299a5a9cb866a48512f5df93bfeec86
Junika Rizki <117917208+junikarizki1@users.noreply.github.com>
2022-12-08 11:08:35 +0700

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+//Looping, jika bernilai True maka akan dieksekuli
+    cout <<"Looping While Created by Junika Rizki\n";
+    cout <<"=====================================\n";
+    int y=8;
+    while (y>=2) {
+        cout <<y <<" ";
+        y--;
+    } cout <<endl;
+
+    y=3;
+    while (y<=21) {
+        cout <<y <<" ";
+        y+=3;
+    } cout <<endl;
+
+    y=40;
+    while (y>=20) {
+       cout <<y <<" ";
+        y-=4;
+    }  cout <<endl;
+
+     y=5;
+    while (y<=45) {
+        cout <<y <<" ";
+        y+=5;
+    } cout <<endl;
+
+    y=90;
+    while (y>=0) {
+       cout <<y <<" ";
+        y-=10;
+    }  cout <<endl;
+
+    int n;
+    cout <<"Masukkan Nilai n ";
+    cin >>n;
+    y=2;
+    while (y<=n) {
+        cout <<y <<" ";
+        y+=2;
+    } cout <<endl;
+
+    return 0;
+}
