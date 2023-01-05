58e4b791d94c8be5a7b8e8598d78104308ebeccb
Junika Rizki <117917208+junikarizki1@users.noreply.github.com>
2023-01-05 21:13:37 +0700

    Create array hari for.cpp

---
+++ array hari for.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+string days [10];
+for (int x=0; x<7; x++){
+    cout <<"Inputkan Hari ";
+    cin >>days[x];
+}
+for (int x=0; x<7; x++){
+    cout <<days[x] <<" ";
+}
+    return 0;
+}
