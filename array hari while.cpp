546ebe4823b24df07c06e44748f62f15c6c9c4f0
Junika Rizki <117917208+junikarizki1@users.noreply.github.com>
2023-01-05 21:19:31 +0700

    Create array hari while.cpp

---
+++ array hari while.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+string days [10];
+int x=0;
+while (x<7){
+    cout <<"Inputkan Hari ";
+    cin >>days[x];
+    x++;
+}
+x=0;
+while (x<7){
+    cout <<days[x] <<" ";
+    x++;
+}
+
+    return 0;
+}
