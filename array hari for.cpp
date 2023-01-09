f4a92da55f2d2ccf09d2feffdfbd4c2e30ce0fe2
Junika Rizki <117917208+junikarizki1@users.noreply.github.com>
2023-01-09 12:51:38 +0700

    Update array hari for.cpp
    rapi dah

--- array hari for.cpp
+++ array hari for.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int main() {
 string days [10];
+cout <<"Menggunakan For Loop \n\n";
 for (int x=0; x<7; x++){
     cout <<"Inputkan Hari ";
     cin >>days[x];
 }
+cout <<endl<<endl;
 for (int x=0; x<7; x++){
     cout <<days[x] <<" ";
 }
